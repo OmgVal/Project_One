bfd5efb7409092cc3cfb063f56dbaca2c9c697fb
OmgValicious <ziallshamrock@gmail.com>
2022-08-22 10:19:51 -0700

    font change

---
+++ main.js
@@ -0,0 +1,11 @@
+/* pseudocode: 
+Step 1: once "play" is clicked game will become active
+Step 2: game will be active until player either 
+    -runs into obstacle -- dies
+    -restarts game
+    -reaches completes lvl 
+Step 3: if player dies make sure to only restart the lvl not the whole game
+
+
+
+
