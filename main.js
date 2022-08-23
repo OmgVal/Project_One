3f3689e2dedfb9b9b2029ed7d4fc0459614b6248
OmgValicious <ziallshamrock@gmail.com>
2022-08-23 15:56:32 -0700

    ball fix

--- main.js
+++ main.js
@@ -12,15 +12,41 @@ const body = document.body
 const gameActive = document.querySelector(".playBtn");
 const endGame = document.querySelector(".restart");
 
-function plyMove()
+// function plyMove()
 
-//collision detection
-function detectHit() {
-    const obstacleLeft = ball.x + ball.width >= obstacleLeft.x
-    const obstacleRight = ball.x <= obstacle.x + obstacle.width
-    const obstacleTop = ball.y + ball.height >= obstacle.y
-    const obstacleBottom = ball.y <= obstacle.y + obstacle.height
+// //collision detection
+// function detectHit() {
+//     const obstacleLeft = ball.x + ball.width >= obstacleLeft.x
+//     const obstacleRight = ball.x <= obstacle.x + obstacle.width
+//     const obstacleTop = ball.y + ball.height >= obstacle.y
+//     const obstacleBottom = ball.y <= obstacle.y + obstacle.height
+//     if (obstacleBottom && obstacleLeft && obstacleRight && obstacleRight) {
+//         ball.alive = false
+//         statusDisplay.innerText = "Game Over!"
+//     }
+// }
+
+//Draggable Ball
+let dragBall;
+
+function move(id) {
+    let obj = document.getElementById('ball');
+    obj.onmousedown = function() {
+    dragBall = obj;
+    }
+    document.onmouseup = function(e) {
+        dragBall = null;
+    }
+    document.onmousemove = function(e) {
+        let x = e.pageX;
+        let y = e.pageY;
+
+        dragBall.style.left = x + "px";
+        dragBall.style.top = y + "px";
+    }
 }
+
+
 
 
 
