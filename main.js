393e52a77f8f5c34c37ca5fd9e7d008e4c9ace6a
OmgValicious <ziallshamrock@gmail.com>
2022-08-25 14:09:56 -0700

    new fuctions invoked

--- main.js
+++ main.js
@@ -9,90 +9,109 @@ Step 3: if player dies make sure to only restart the lvl not the whole game
 */
 
 const body = document.body
-const gameActive = document.querySelector(".playBtn");
-const endGame = document.querySelector(".restart");
-const cursor = document.querySelector('.cursor');
-const mazePath = document.querySelectorAll('.maze');
-const instructions = document.querySelector('.infoBtn').addEventListener("click", displayMsg);
-const obstacle = document.querySelectorAll('.mazeBorder');
-const gameState = document.querySelector('.text')
-const mouseStart = 
+let endGame = document.querySelector(".restart");
+let cursor = document.querySelector('.cursor');
+let mazePath = document.querySelector('.maze');
+let instructions = document.querySelector('.infoBtn').addEventListener("click", displayMsg);
+let obstacle = document.querySelector('.obstacle');
+let gameState = document.querySelector('.text')
+
+
+//move obstacle
+function move() {
+    let moved = false;
+
+    moved ? (obstacle.style.left = '78vw') : (obstacle.style.left = '80vw');
+}
+setInterval(move, 2000)
+
 
 //info button
 function displayMsg() {
-   console.log('itworks!')
-
+    console.log('itworks!')
+    
 }
 
-document.addEventListener("click", e =>{
-    screenX 
-    e = true
-    if (e = true) {
-        //Cursor to Ball
-        document.addEventListener('mousemove', e => {
-            cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
-            // cursor.setAttribute.style.left = (e.pageX - 25) + "px;";
-            // cursor.setAttribute.style.top = (e.pageY - 25) + "px;";
-        })
-
-    } else {
-        document.removeEventListener('mousemove', e => {
-            cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
-        })
-    }
-
-    while (e = true) {
-    }
-
-    console.log('start!')
-})
-
-
-//collision detection
-function detectHit() {
-    const obstacleLeft = cursor.x + cursor.width >= obstacle.x
-    const obstacleRight = cursor.x <= obstacle.x + obstacle.width
-    const obstacleTop = cursor.y + cursor.height >= obstacle.y
-    const obstacleBottom = cursor.y <= obstacle.y + obstacle.height
-    if (obstacleBottom && obstacleLeft && obstacleTop && obstacleRight) {
-        cursor.alive = true
-        console.log('alive')
-     } else {
-        console.log('lost')
-     }
+//turn into ball
+function ballCursor() {
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
+    })
+    
 }
+
+// //collision detection
+// function detectHit() {
+//     const obstacleLeft = cursor.x + cursor.width >= obstacle.x
+//     const obstacleRight = cursor.x <= obstacle.x + obstacle.width
+//     const obstacleTop = cursor.y + cursor.height >= obstacle.y
+//     const obstacleBottom = cursor.y <= obstacle.y + obstacle.height
+//     if (obstacleBottom && obstacleLeft && obstacleTop && obstacleRight) {
+//         cursor.alive = true
+//         console.log('alive')
+//      } else {
+//         console.log('lost')
+//      }
+// }
+
+function gameStart() {
+    move()
+    ballCursor()
+    detectHit()
+}
+
 // console.log(detectHit())
 
-// //Draggable Ball
-// let dragBall;
+// document.addEventListener("click", e =>{
+//     const gameActive = document.querySelector(".start");
+//     e = true
+//     if (e = true) {
+//         //Cursor to Ball
+//         document.addEventListener('mousemove', e => {
+//             cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
+//             // cursor.setAttribute.style.left = (e.pageX - 25) + "px;";
+//             // cursor.setAttribute.style.top = (e.pageY - 25) + "px;";
+//         })
+//         detectHit()
+//     } else {
+//         document.removeEventListener('mousemove', e => {
+//             cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
+//         })
+//     }
+// })
 
-// function move(id) {
-    //     let obj = document.getElementById('ball');
-    //     obj.onmousedown = function() {
-        //         dragBall = obj;
-        //     }
-        //     document.onmouseup = function(e) {
-            //         dragBall = null;
-            //     }
-            //     document.onmousemove = function(e) {
-                //         let x = e.pageX;
-                //         let y = e.pageY;
+
+
+// // //Draggable Ball
+// // let dragBall;
+
+// // function move(id) {
+//     //     let obj = document.getElementById('ball');
+//     //     obj.onmousedown = function() {
+//         //         dragBall = obj;
+//         //     }
+//         //     document.onmouseup = function(e) {
+//             //         dragBall = null;
+//             //     }
+//             //     document.onmousemove = function(e) {
+//                 //         let x = e.pageX;
+//                 //         let y = e.pageY;
                 
-                //         dragBall.style.left = x + "px;";
-                //         dragBall.style.top = y + "px;";
-                //     }
-                // }
+//                 //         dragBall.style.left = x + "px;";
+//                 //         dragBall.style.top = y + "px;";
+//                 //     }
+//                 // }
                 
                 
  
                 
-gameActive.addEventListener("click", e => {
-    detectHit()
+// gameActive.addEventListener("click", e => {
+//     detectHit()
 
- })
+//  })
 
- window.addEventListener("mouseover", (e) => {
-    console.log(e.clientX, e.clientY)
- })
+//  window.addEventListener("mouseover", (e) => {
+//     console.log(e.clientX, e.clientY)
+//  })
 
 //  change cursor loc upon starting game to inside maze -- click on maze starts game
