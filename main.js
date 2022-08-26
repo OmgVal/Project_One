5e760e2361e5d912e3c5652d78f2c9736ddc97e2
OmgValicious <ziallshamrock@gmail.com>
2022-08-25 20:32:02 -0700

    modal complete

--- main.js
+++ main.js
@@ -9,83 +9,121 @@ Step 3: if player dies make sure to only restart the lvl not the whole game
 */
 
 const body = document.body
+const openModalBtn = document.querySelectorAll('[data-modal-target]')
+const closeModalBtn = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 let endGame = document.querySelector(".restart");
 let cursor = document.querySelector('.cursor');
 let mazePath = document.querySelector('.maze');
-let instructions = document.querySelector('.infoBtn').addEventListener("click", displayMsg);
+// let instructions = document.querySelector('.infoBtn').addEventListener("click", displayMsg);
 let obstacle = document.querySelector('.obstacle');
 let gameState = document.querySelector('.text')
 
 
-//move obstacle
-function move() {
-    let moved = false;
-    moved ? (moved = false, obstacle.style.left = '78vw') : (moved = true, obstacle.style.left = '80vw');
-}
-setInterval(move, 6000)
-
-
-//info button
-function displayMsg() {
-    console.log('itworks!')
-    
-}
-
-//collision detection
-const detectHit = (value) => {
-    console.log(mazePath.classList.value, value)
-    const mazeClass = mazePath.classList.value
-    // console.log(value)
-    if (value === mazeClass) {
-        console.log('alive')
-    } else{
-        // gameOver()
-        console.log('Game Over')
-
-    };
-    if (value === "finish") {
-        console.log('finish')
-    };
-    // const obstacleLeft = cursor.x + cursor.width >= obstacle.x
-    // const obstacleRight = cursor.x <= obstacle.x + obstacle.width
-    // const obstacleTop = cursor.y + cursor.height >= obstacle.y
-    // const obstacleBottom = cursor.y <= obstacle.y + obstacle.height
-    // if (obstacleBottom && obstacleLeft && obstacleTop && obstacleRight) {
-    //     cursor.alive = true
-    //     console.log('alive')
-    // } else {
-    //     console.log('lost')
-    // }
-};
-
-//turn into ball
-function ballCursor() {
-    document.addEventListener('mousemove', e => {
-        cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
-        let check = e.target.classList.value;
-        detectHit(check);
+//Modal
+openModalBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
     })
-    
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = button.closest('.infoBox')
+        closeModal(modal)
+    })
+});
+
+function openModal(modal) {
+    if(modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
 
 
+// //move obstacle
+// function move() {
+//     let moved = false;
+//     moved ? (moved = false, obstacle.style.left = '78vw') : (moved = true, obstacle.style.left = '80vw');
+// }
+// setInterval(move, 6000)
 
-// //Game Over
-// function gameOver {
-    //     if 
-    // }
+
+// //info button
+// function displayMsg() {
+//     console.log('itworks!')
     
-    
-//  window.addEventListener("mousemove", e => {
+// }
+
+// //collision detection
+// const detectHit = (value) => {
+//     console.log(mazePath.classList.value, value)
+//     const mazeClass = mazePath.classList.value
+//     // console.log(value)
+//     if (value === obstacle) {
+//         console.log('dead')
+//     } else{
+//         // gameOver()
+//         console.log('Game Over')
+
+//     };
+//     if (value === "finish") {
+//         console.log('finish')
+//     };
+//     // const obstacleLeft = cursor.x + cursor.width >= obstacle.x
+//     // const obstacleRight = cursor.x <= obstacle.x + obstacle.width
+//     // const obstacleTop = cursor.y + cursor.height >= obstacle.y
+//     // const obstacleBottom = cursor.y <= obstacle.y + obstacle.height
+//     // if (obstacleBottom && obstacleLeft && obstacleTop && obstacleRight) {
+//     //     cursor.alive = true
+//     //     console.log('alive')
+//     // } else {
+//     //     console.log('lost')
+//     // }
+// };
+
+// //turn into ball
+// function ballCursor() {
+//     document.addEventListener('mousemove', e => {
+//         cursor.setAttribute('style', "top: " + (e.pageY - 25) + 'px; left: ' + (e.pageX - 25) + 'px;')
 //         let check = e.target.classList.value;
 //         detectHit(check);
-// })
+//     })
     
-function gameStart() {
-    move()
-    ballCursor()
-    // detectHit()
-}
+// }
+
+
+
+// // //Game Over
+// // function gameOver {
+//     //     if 
+//     // }
+    
+    
+// //  window.addEventListener("mousemove", e => {
+// //         let check = e.target.classList.value;
+// //         detectHit(check);
+// // })
+    
+// function gameStart() {
+//     move()
+//     ballCursor()
+//     // detectHit()
+// }
 
 // console.log(detectHit())
 
